1edd9a76686b2f2d66be23628627b875491c35d4
Ahmed Aslam <ahmedaslam2004@gmail.com>
2025-04-22 23:41:51 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -51,5 +51,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
